afb52f0d9fdb6873d9a36b54e94097f802445e77
KitsuneNoctus <theceintiant@gmail.com>
2020-01-08 22:05:38 -0800

    Adding new panels. Doing best to create clean look and understand what is to happen.

--- Defuse/AnxietyViewController.swift
+++ Defuse/AnxietyViewController.swift
@@ -47,23 +47,6 @@ class AnxietyViewController: UIViewController {
                  })
     }
     
-//    func setBackgroundColor(){
-//        let colors = [
-//            UIColor(red: 233/255, green: 203/255, blue: 198/255, alpha: 1),
-//            UIColor(red: 38/255, green: 188/255, blue: 192/255, alpha: 1),
-//            UIColor(red: 253/255, green: 221/255, blue: 164/255, alpha: 1),
-//            UIColor(red: 235/255, green: 154/255, blue: 171/255, alpha: 1),
-//            UIColor(red: 87/255, green: 141/255, blue: 155/255, alpha: 1)
-//        ]
-//        let randomColor = Int(arc4random_uniform(UInt32 (colors.count)))
-//        view.backgroundColor = colors[randomColor]
-//    }
-//
-//    func updateColor(){
-//
-//        view.backgroundColor = UIColor(red:CGFloat.random(in:0..<1),green:0,blue:0.5,alpha: 1)
-//    }
-    
 
     /*
     // MARK: - Navigation
