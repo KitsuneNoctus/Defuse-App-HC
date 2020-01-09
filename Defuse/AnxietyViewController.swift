4039971143a68d828bed54684abd39c2282c7ff1
KitsuneNoctus <theceintiant@gmail.com>
2020-01-08 21:32:41 -0800

    background color changes over time.

--- Defuse/AnxietyViewController.swift
+++ Defuse/AnxietyViewController.swift
@@ -7,16 +7,23 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AnxietyViewController: UIViewController {
     
     //Timer
-    var timerTic:Timer!
+    var timer: Timer?
+    var timeLeft: Double = 30
+//    var timerTic:Timer!
+//    var num = 0.1
+//    var timeLike = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        timeBackgroundChanger()
+        
 //        timerTic = Timer.scheduledTimer(timeInterval: 1, target: self, selector: Selector(("setBackgroundColor")), userInfo: nil, repeats: true)
-        self.setBackgroundColor()
+        //self.setBackgroundColor()
 //        updateColor()
         
 //        view.backgroundColor = UIColor(red:CGFloat.random(in:0..<1),green:0,blue:0.5,alpha: 1)
@@ -24,22 +31,36 @@ class AnxietyViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    func setBackgroundColor(){
-        let colors = [
-            UIColor(red: 233/255, green: 203/255, blue: 198/255, alpha: 1),
-            UIColor(red: 38/255, green: 188/255, blue: 192/255, alpha: 1),
-            UIColor(red: 253/255, green: 221/255, blue: 164/255, alpha: 1),
-            UIColor(red: 235/255, green: 154/255, blue: 171/255, alpha: 1),
-            UIColor(red: 87/255, green: 141/255, blue: 155/255, alpha: 1)
-        ]
-        let randomColor = Int(arc4random_uniform(UInt32 (colors.count)))
-        view.backgroundColor = colors[randomColor]
+    func timeBackgroundChanger(){
+        var colorVal:Double = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { timerChange in
+            colorVal += 0.1
+                    if self.timeLeft <= 0{
+                        self.timeLeft = 30
+                        colorVal = 0
+        
+                    }
+                    self.timeLeft -= 1
+            self.view.backgroundColor = UIColor(red: CGFloat(colorVal), green: 1-CGFloat(colorVal), blue: CGFloat(colorVal), alpha: 1)
+                 })
     }
     
-    func updateColor(){
-        
-        view.backgroundColor = UIColor(red:CGFloat.random(in:0..<1),green:0,blue:0.5,alpha: 1)
-    }
+//    func setBackgroundColor(){
+//        let colors = [
+//            UIColor(red: 233/255, green: 203/255, blue: 198/255, alpha: 1),
+//            UIColor(red: 38/255, green: 188/255, blue: 192/255, alpha: 1),
+//            UIColor(red: 253/255, green: 221/255, blue: 164/255, alpha: 1),
+//            UIColor(red: 235/255, green: 154/255, blue: 171/255, alpha: 1),
+//            UIColor(red: 87/255, green: 141/255, blue: 155/255, alpha: 1)
+//        ]
+//        let randomColor = Int(arc4random_uniform(UInt32 (colors.count)))
+//        view.backgroundColor = colors[randomColor]
+//    }
+//
+//    func updateColor(){
+//
+//        view.backgroundColor = UIColor(red:CGFloat.random(in:0..<1),green:0,blue:0.5,alpha: 1)
+//    }
     
 
     /*
