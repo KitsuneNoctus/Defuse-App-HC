fd2d25d50bf9194b2b7382e4593c5aa01dc4bb0d
Henry Calderon <theceintiant@gmail.com>
2021-01-07 13:09:37 -0800

    Home view completed, starting on fidget view

--- Defuse/Controllers/FidgetToolViewController.swift
+++ Defuse/Controllers/FidgetToolViewController.swift
@@ -7,24 +7,64 @@
 //
 
 import UIKit
+import AVFoundation
 
 class FidgetToolViewController: UIViewController {
+    
+    let switchOne: UISwitch = {
+        let swit = UISwitch()
+        return swit
+    }()
+    
+    let stepOne: UIStepper = {
+        let step = UIStepper()
+        return step
+    }()
+    
+    let sliderOne: UISlider = {
+        let slider = UISlider()
+        return slider
+    }()
+    
+    let segmentControl: UISegmentedControl = {
+        let segment = UISegmentedControl()
+        return segment
+    }()
+    
+    let aButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    let aLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+//    let keyView = UIView {
+//        let view = UIView()
+//        return view
+//    }()
+//    
+//    let bubbleView = UIView {
+//        let view = UIView()
+//        return view
+//    }()
+    
+    let aLabelWordList = ["They're","There","Their","your","you're","bear","bare","angle","angel"]
+    
+    var soundEffect: AVAudioPlayer?
+    var redValue = CGFloat.random(in:0..<1)
 
+    //MARK: View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: Setup
+    func setup(){
+        
     }
-    */
 
 }
--- Defuse/Controllers/HomeViewController.swift
+++ Defuse/Controllers/HomeViewController.swift
@@ -13,35 +13,45 @@ class HomeViewController: UIViewController {
     let fidgetButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.titleLabel?.text = "Fidget"
+        button.setTitle("Fidget", for: .normal)
+        button.setTitleColor(.systemGray, for: .normal)
+        button.addTarget(self, action: #selector(navFidget), for: .touchUpInside)
         return button
     }()
     
     let notesButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.titleLabel?.text = "Notes"
+        button.setTitle("Notes", for: .normal)
+        button.setTitleColor(.systemGray, for: .normal)
+        button.addTarget(self, action: #selector(navNotes), for: .touchUpInside)
         return button
     }()
     
     let angerButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.titleLabel?.text = "Anger"
+        button.setTitle("Anger", for: .normal)
+        button.setTitleColor(.systemGray, for: .normal)
+        button.addTarget(self, action: #selector(navAnger), for: .touchUpInside)
         return button
     }()
     
     let anxietyButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.titleLabel?.text = "Anxiety"
+        button.setTitle("Anxiety", for: .normal)
+        button.setTitleColor(.systemGray, for: .normal)
+        button.addTarget(self, action: #selector(navAnxiety), for: .touchUpInside)
         return button
     }()
     
     let moreButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.titleLabel?.text = "More"
+        button.setTitle("More", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(navMore), for: .touchUpInside)
         return button
     }()
     
@@ -72,19 +82,49 @@ class HomeViewController: UIViewController {
         setup()
     }
     
+    //MARK: Setup
     func setup(){
         self.view.addSubview(titleLabel)
         self.view.addSubview(buttonStack)
         buttonStack.addArrangedSubview(fidgetButton)
-//        buttonStack.addArrangedSubview(angerButton)
+        buttonStack.addArrangedSubview(notesButton)
+        buttonStack.addArrangedSubview(angerButton)
+        buttonStack.addArrangedSubview(anxietyButton)
+        buttonStack.addArrangedSubview(moreButton)
         
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50),
             titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             
             buttonStack.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 40),
-//            buttonStack.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+            buttonStack.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
         ])
+    }
+    
+    //MARK: Actions / Nav
+    @objc func navFidget(){
+        let nextVC = FidgetToolViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    @objc func navNotes(){
+        let nextVC = NotesViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    @objc func navAnger(){
+        let nextVC = EmotionViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    @objc func navAnxiety(){
+        let nextVC = EmotionViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    @objc func navMore(){
+        let nextVC = EmotionViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
     }
 
 }
